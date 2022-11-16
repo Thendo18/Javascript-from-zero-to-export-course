025f4860bddd770791665eda192a351bc1556bb4
Thendo <thendom@touchsides.com>
2022-11-16 09:33:40 +0200

    activities

--- 02-Fundamentals-Part-2/starter/challenge1.js
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -1,23 +1,16 @@
 const calAverage = (a, b, c) =>(a+b+c)/3;
-// console.log(calAverage(1,2,3))
-
-
-// test1
-
 const scoreDolphin = calAverage(44,23,71);
-    const scorekoalas = calAverage(65,54,49);
+const scorekoalas = calAverage(65,54,49);
 
-    const avgDolphin = scoreDolphin;
-    const avgKoalas = scorekoalas;
-
-function checkWinner(avgDolphin ,avgKoalas){
-
-    avgDolphin>avgKoalas ? 
-    console.log(`Dolphins wins (${avgDolphin} vs. ${avgKoalas})`):
-    console.log(`koalas wins (${avgDolphin} vs. ${avgKoalas})`)
+const checkWinner = function (avgDolphin ,avgKoalas){
+    if(avgDolphin>avgKoalas){
+    console.log(`Dolphins wins (${avgDolphin} vs. ${avgKoalas})`)}
+    else{
+    console.log(`koalas wins (${avgDolphin} vs. ${avgKoalas})`)}
  
 }
-// console.log(scoreDolphin,  scorekoalas);
+
+checkWinner(scoreDolphin,scorekoalas);
 
 
 
---
+++ 02-Fundamentals-Part-2/starter/exercise.js
@@ -0,0 +1,5 @@
+const years = [2000, 2001, 2002, 2003, 2004, 2005]
+console.log(years)
+console.log(years.length)
+
+
